000e576764253fb2d2f64eaf59c851deb8d66502
Matthias Vernimme <vernimme.matthias@gmail.com>
2024-05-02 16:52:13 +0200

    Correct angle blinks

--- Assets/csharp/GameLogic.cs
+++ Assets/csharp/GameLogic.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a65255fa8c9d656f34db0bc961a97b55289852295075557e904c35ddbf4ce5b
-size 4383
+oid sha256:068b0aedbc7fc0b87e0ce015cb42b0f971f07ac0509862400d6817d9d7c2fe43
+size 5931
