753470c9fa1654e26bee73d592fe965408fbbba2
Joris Machon <joris.machon1@gmail.com>
2024-05-02 16:10:14 +0200

    fixed inversion of rotation

--- Assets/csharp/GameLogic.cs
+++ Assets/csharp/GameLogic.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:190a0a2f22a3db48cb2d132b31d5d66232dc17a1c3d56f9cf7f9b7d1c59301d0
-size 4149
+oid sha256:0a65255fa8c9d656f34db0bc961a97b55289852295075557e904c35ddbf4ce5b
+size 4383
