75acfbc0af5c92dfb20658ea4e77f78e27382cda
Segawa Shusaku <segashu0214@gmail.com>
2024-05-16 17:59:10 +0200

    Logging function added

--- Assets/csharp/ButtonHandler.cs
+++ Assets/csharp/ButtonHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:392b6a6ab8113ac1816c26809506d5667a9c37e5d59d20bdbbf6f4ea157ddc5e
-size 1211
+oid sha256:6ebca37b62998fe968f1921d96e57e2f7771a31929e892cb4faf16048c7d6a8d
+size 2776
--- Assets/csharp/DistanceController.cs
+++ Assets/csharp/DistanceController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8c977bc9e2e1c1bdfdfd02ac627ab8367b9301b406354368fa7f158ce399735
-size 1950
+oid sha256:f3100ce5acbbf5c255b4c285627cdb0d06c3de97ffc54429da7718bc1e1c374a
+size 1782
