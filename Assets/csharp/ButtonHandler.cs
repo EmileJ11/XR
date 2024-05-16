d9b3cc29884ea7e8e5c07db194651a56916658bf
Segawa Shusaku <segashu0214@gmail.com>
2024-05-16 17:03:58 +0200

    Created a new scene DistanceMode.

---
+++ Assets/csharp/ButtonHandler.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:392b6a6ab8113ac1816c26809506d5667a9c37e5d59d20bdbbf6f4ea157ddc5e
+size 1211
---
+++ Assets/csharp/DistanceController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f8c977bc9e2e1c1bdfdfd02ac627ab8367b9301b406354368fa7f158ce399735
+size 1950
