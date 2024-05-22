67af701ce607bf9fb670ab1f16af6e00bec03310
Segawa Shusaku <segashu0214@gmail.com>
2024-05-22 14:37:53 +0200

    The game starts after a learning mode. Game stops after 20 clicks.

--- Assets/csharp/ButtonHandler.cs
+++ Assets/csharp/ButtonHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ebca37b62998fe968f1921d96e57e2f7771a31929e892cb4faf16048c7d6a8d
-size 2776
+oid sha256:6047380ce6936424e252fe8a094332cb074cbedf2ab0e8d4a3c126c4e6b0ff6b
+size 6947
--- Assets/csharp/DistanceController.cs
+++ Assets/csharp/DistanceController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3100ce5acbbf5c255b4c285627cdb0d06c3de97ffc54429da7718bc1e1c374a
-size 1782
+oid sha256:070c26d819beed17811beaffc4cfafc5f53b24ec02d46527fb9b2201ac974358
+size 2410
